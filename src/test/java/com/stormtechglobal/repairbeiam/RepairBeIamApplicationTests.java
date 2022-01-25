50147ee2067415d03690db49222f098eedb26e5f
Andalib <andalibquraishi14@gmail.com>
2022-01-24 20:22:00 -1200

    second commit

---
+++ src/test/java/com/stormtechglobal/repairbeiam/RepairBeIamApplicationTests.java
@@ -0,0 +1,11 @@
+package com.stormtechglobal.repairbeiam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairBeIamApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
